9aa0afcc5acd5d97f493e83b7529bbc9f4a30d2a
Arief Maulana Rizki <ariefmaulanarizki5@gmail.com>
2025-12-12 14:52:09 +0700

    update excel export

--- app/Exports/AbsenExport.php
+++ app/Exports/AbsenExport.php
@@ -17,12 +17,29 @@ class AbsenExport implements FromCollection, WithHeadings
     public function collection()
     {
         return $this->data->map(function ($item) {
+
+            // Hitung total waktu kerja
+            if ($item->check_in && $item->check_out) {
+                $in = strtotime($item->check_in);
+                $out = strtotime($item->check_out);
+                $diff = $out - $in;
+
+                $hours = floor($diff / 3600);
+                $minutes = floor(($diff % 3600) / 60);
+
+                $totalTime = sprintf('%02d jam %02d menit', $hours, $minutes);
+            } else {
+                $totalTime = '-';
+            }
+
+            // Data export
             return [
-                'Nama User' => $item->user->name,
-                'Tanggal' => $item->scanned_at,
-                'Masuk' => $item->check_in,
-                'Keluar' => $item->check_out,
-                'Catatan' => $item->note,
+                'Nama User'     => $item->user->name,
+                'Tanggal'       => $item->scanned_at,
+                'Masuk'         => $item->check_in,
+                'Keluar'        => $item->check_out,
+                'Total Waktu'   => $totalTime,
+                'Catatan'       => $item->note,
             ];
         });
     }
@@ -34,6 +51,7 @@ class AbsenExport implements FromCollection, WithHeadings
             'Tanggal',
             'Masuk',
             'Keluar',
+            'Total Waktu',
             'Catatan',
         ];
     }
